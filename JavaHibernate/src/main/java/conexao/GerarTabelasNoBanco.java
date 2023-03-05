b2c91eee4f583dd8f634b2466e70e8c110cb6dd9
Leo Trevisol <104337845+Leo-Trevisol@users.noreply.github.com>
2023-03-05 00:46:11 -0300

    tentando exportar banco via hibernate

---
+++ JavaHibernate/src/main/java/conexao/GerarTabelasNoBanco.java
@@ -0,0 +1,27 @@
+package conexao;
+
+import org.eclipse.persistence.internal.oxm.schema.model.Schema;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.cfg.Configuration;
+
+import bean.Aluno;
+import bean.Pessoa;
+
+public class GerarTabelasNoBanco {
+	
+	public static void main(String[] args) {
+		
+		MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+		metadataSources.addAnnotatedClass(Aluno.class);
+		metadataSources.addAnnotatedClass(Pessoa.class);
+		Metadata metadata = metadataSources.buildMetadata();
+
+		Schema schemaExport = new Schema();
+		schemaExport.setFormat(true);
+		schemaExport.setOutputFile("create.sql");
+		schemaExport.createOnly(EnumSet.of(TargetType.SCRIPT), metadata);
+	}
+	
+	
+}
